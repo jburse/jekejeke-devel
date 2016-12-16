e4d63c928e509a5bd5656debe4e1c7d254f2684d
Jan Burse <janburse@fastmail.fm>
2016-12-16 21:47:45 +0100

    Launch Jekejeke Prolog 1.1.8

--- jekrun/headless/jekpro/reference/structure/SpecialType.java
+++ jekrun/headless/jekpro/reference/structure/SpecialType.java
@@ -14,9 +14,29 @@ import java.math.BigInteger;
 
 /**
  * <p>Provides built-in predicates for type checking.</p>
- *
- * @author Copyright 1985-2014, XLOG Technologies GmbH, Switzerland
- * @version Jekejeke Prolog 0.9.2 (a fast and small prolog interpreter)
+ * <p/>
+ * Warranty & Liability
+ * To the extent permitted by applicable law and unless explicitly
+ * otherwise agreed upon, XLOG Technologies GmbH makes no warranties
+ * regarding the provided information. XLOG Technologies GmbH assumes
+ * no liability that any problems might be solved with the information
+ * provided by XLOG Technologies GmbH.
+ * <p/>
+ * Rights & License
+ * All industrial property rights regarding the information - copyright
+ * and patent rights in particular - are the sole property of XLOG
+ * Technologies GmbH. If the company was not the originator of some
+ * excerpts, XLOG Technologies GmbH has at least obtained the right to
+ * reproduce, change and translate the information.
+ * <p/>
+ * Reproduction is restricted to the whole unaltered document. Reproduction
+ * of the information is only allowed for non-commercial uses. Selling,
+ * giving away or letting of the execution of the library is prohibited.
+ * The library can be distributed as part of your applications and libraries
+ * for execution provided this comment remains unchanged.
+ * <p/>
+ * Trademarks
+ * Jekejeke is a registered trademark of XLOG Technologies GmbH.
  */
 public final class SpecialType extends Special {
     private final static int SPECIAL_INTEGER = 0;
