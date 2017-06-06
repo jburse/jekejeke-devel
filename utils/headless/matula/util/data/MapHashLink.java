b5cf8f5ab75b2374d66f3da85527e0ecd33b2b22
Jan Burse <janburse@fastmail.fm>
2017-06-06 18:31:03 +0200

    Launch Jekejeke Prolog 1.2.2

--- utils/headless/matula/util/data/MapHashLink.java
+++ utils/headless/matula/util/data/MapHashLink.java
@@ -2,9 +2,29 @@ package matula.util.data;
 
 /**
  * <p>Implementation of a linked hash map.</p>
- *
- * @author Copyright 2012-2016, XLOG Technologies GmbH, Switzerland
- * @version Jekejeke Prolog 0.9.6 (a fast and small prolog interpreter)
+ * </p>
+ * Warranty & Liability
+ * To the extent permitted by applicable law and unless explicitly
+ * otherwise agreed upon, XLOG Technologies GmbH makes no warranties
+ * regarding the provided information. XLOG Technologies GmbH assumes
+ * no liability that any problems might be solved with the information
+ * provided by XLOG Technologies GmbH.
+ * <p/>
+ * Rights & License
+ * All industrial property rights regarding the information - copyright
+ * and patent rights in particular - are the sole property of XLOG
+ * Technologies GmbH. If the company was not the originator of some
+ * excerpts, XLOG Technologies GmbH has at least obtained the right to
+ * reproduce, change and translate the information.
+ * <p/>
+ * Reproduction is restricted to the whole unaltered document. Reproduction
+ * of the information is only allowed for non-commercial uses. Selling,
+ * giving away or letting of the execution of the library is prohibited.
+ * The library can be distributed as part of your applications and libraries
+ * for execution provided this comment remains unchanged.
+ * <p/>
+ * Trademarks
+ * Jekejeke is a registered trademark of XLOG Technologies GmbH.
  */
 public class MapHashLink<K, V> extends AbstractMap<K, V> {
     private static final int MIN_SIZE = 2;
