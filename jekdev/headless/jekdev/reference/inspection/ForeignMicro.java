fd3262c4374e1e016dd7b11a987114d84d56d10d
Jan Burse <janburse@fastmail.fm>
2019-03-08 11:19:14 +0100

    Launch Jekejeke Prolog 1.3.6

--- jekdev/headless/jekdev/reference/inspection/ForeignMicro.java
+++ jekdev/headless/jekdev/reference/inspection/ForeignMicro.java
@@ -1,69 +1,72 @@
+package jekdev.reference.inspection;
+
+import jekpro.tools.call.CallIn;
+import jekpro.tools.call.Interpreter;
+import jekpro.tools.call.InterpreterException;
+import jekpro.tools.call.InterpreterMessage;
+import jekpro.tools.term.AbstractTerm;
+
 /**
- * This module provides an interface to micro engines. Micro engines
- * share the same controller with its parent. A new micro engine can be
- * created by the predicate micro_new/2. A micro engine can then be
- * controlled by the predicates micro_cont/1 and micro_close/1.
- *
+ * <p>Provides built-in predicates for the module micro.</p>
+ * <p/>
  * Warranty & Liability
  * To the extent permitted by applicable law and unless explicitly
  * otherwise agreed upon, XLOG Technologies GmbH makes no warranties
  * regarding the provided information. XLOG Technologies GmbH assumes
  * no liability that any problems might be solved with the information
  * provided by XLOG Technologies GmbH.
- *
+ * <p/>
  * Rights & License
  * All industrial property rights regarding the information - copyright
  * and patent rights in particular - are the sole property of XLOG
  * Technologies GmbH. If the company was not the originator of some
  * excerpts, XLOG Technologies GmbH has at least obtained the right to
  * reproduce, change and translate the information.
- *
+ * <p/>
  * Reproduction is restricted to the whole unaltered document. Reproduction
  * of the information is only allowed for non-commercial uses. Selling,
  * giving away or letting of the execution of the library is prohibited.
  * The library can be distributed as part of your applications and libraries
  * for execution provided this comment remains unchanged.
- *
+ * <p/>
  * Restrictions
  * Only to be distributed with programs that add significant and primary
  * functionality to the library. Not to be distributed with additional
  * software intended to replace any components of the library.
- *
+ * <p/>
  * Trademarks
  * Jekejeke is a registered trademark of XLOG Technologies GmbH.
  */
+public final class ForeignMicro {
 
-:- package(library(jekpro/frequent/advanced)).
-:- use_package(foreign(jekpro/frequent/advanced)).
-:- use_package(foreign(matula/util/data)).
-:- use_package(foreign(jekpro/tools/call)).
-:- use_package(foreign(jekpro/tools/term)).
+    /**
+     * <p>Create a new micro engine.</p>
+     *
+     * @param inter The interpreter.
+     * @param goal  The goal.
+     * @return The micro engine.
+     */
+    public static CallIn sysMicroNew(Interpreter inter, AbstractTerm goal) {
+        Object obj = AbstractTerm.copyMolec(inter, goal);
+        return inter.iterable().iterator(obj);
+    }
 
-:- module(micro, []).
+    /**
+     * <p>Continue with micro engine.</p>
+     *
+     * @param callin The micro engine.
+     * @return True if the continuation succeeded, otherwise false.
+     * @throws InterpreterException Shit happens.
+     * @throws InterpreterMessage   No such element.
+     */
+    public static boolean sysMicroCont(CallIn callin)
+            throws InterpreterException, InterpreterMessage {
+        if (callin.hasNext()) {
+            callin.next();
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-/**
- * micro_new(G, M):
- * The predicate succeeds in M with a new micro engine for the goal G.
- */
-% micro_new(+Goal, -Micro)
-:- public micro_new/2.
-:- foreign(micro_new/2, 'ForeignMicro', sysMicroNew('Interpreter','AbstractTerm')).
-
-/**
- * micro_cont(M):
- * The predicate succeeds if the micro engine could continue to a success.
- */
-% micro_cont(+Micro)
-:- public micro_cont/1.
-:- foreign(micro_cont/1, 'ForeignMicro', sysMicroCont('CallIn')).
-
-/**
- * micro_close(M):
- * The predicate succeeds in closing the micro engine M.
- */
-% micro_close(+Micro)
-:- public micro_close/1.
-:- virtual micro_close/1.
-:- foreign(micro_close/1, 'CallIn', close).
-
-
+}
